ac5786a5043749afd79b393a6e5ca66e0dea1ffc
Eternal Juggernaut Servitor <playtejasz1996@gmail.com>
2016-10-30 22:59:49 +0530

    Update manga.sh

--- manga.sh
+++ manga.sh
@@ -3,14 +3,14 @@
 for x in `seq 26 850`;
 do
 
-mkdir ep$x
+mkdir ep$x #make directory for a new episode
 cd ep$x
 
 y=1 
 
 wget http://www2.watchop.io/manga2/read/one-piece/$x/$y -q -O pagesource
 
-limit=$(grep of... pagesource -o | awk '{print $2}')
+limit=$(grep of... pagesource -o | awk '{print $2}') #extract the number of scenes in the episode
 
 echo this episode has : $limit
 
@@ -19,12 +19,12 @@ do
 
 echo waiting on $x and $y
 
-wget http://www2.watchop.io/manga2/read/one-piece/$x/$y -q -O pagesource 
+wget http://www2.watchop.io/manga2/read/one-piece/$x/$y -q -O pagesource #get the source code of the page of episode $x and scene $y 
 
 wait
 
 wget `grep src.*mangareader.net/one-piece/.*/one-piece-.*.jpg pagesource -o | sed 's/^\(src="\)*//'` -q -O ep$x\_scene$y.jpg
- 
+#extract and download the image
 echo 
 echo 
 
